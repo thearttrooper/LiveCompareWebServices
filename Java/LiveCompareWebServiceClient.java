362e8bbe1c270d3ee077e96b36043671e156592b
TheArtTrooper <ctrueman@intellicorp.com>
2017-01-18 12:08:43 +0000

    Client version 1 - simple hello world.

---
+++ Java/LiveCompareWebServiceClient.java
@@ -0,0 +1,10 @@
+import java.net.URL;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import com.intellicorp.schemas.livecompare.*;
+
+public class LiveCompareWebServiceClient {
+    public static void main(String[] args) throws Exception {
+	System.out.println("Hello, World!");
+    }
+}
