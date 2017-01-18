515e396cf05cf8182f3d16ebdbc26addda4527e6
TheArtTrooper <ctrueman@intellicorp.com>
2017-01-18 12:15:38 +0000

    Client version 2 - add service setup/call/polling.

--- Java/LiveCompareWebServiceClient.java
+++ Java/LiveCompareWebServiceClient.java
@@ -5,6 +5,33 @@ import com.intellicorp.schemas.livecompare.*;
 
 public class LiveCompareWebServiceClient {
     public static void main(String[] args) throws Exception {
-	System.out.println("Hello, World!");
+	GetTableContents service = new GetTableContents();
+	GetTableContentsSoap soap = service.getGetTableContentsSoap();
+	AsynchronousStartResult start_result = soap.startWorkflow("T001");
+	String token = start_result.getToken();
+	int interval = start_result.getInterval();
+	Status status = Status.UNRECOGNIZED_TOKEN;
+	AsynchronousEndResult end_result = soap.endWorkflow(token);
+	boolean finished = false;
+
+	while (true) {
+	    status = end_result.getStatus();
+
+	    System.out.println(status.name());
+
+	    if (status == Status.FAILED ||
+		status == Status.COMPLETED ||
+		status == Status.UNRECOGNIZED_TOKEN) {
+		break;
+	    }
+
+	    System.out.format("Sleeping for %ds...\n", interval);
+	    Thread.sleep(interval * 1000);
+
+	    // NEVER call endWorkflow() unless in QUEUED or RUNNING
+	    // state. If you call it, for example, once COMPLETED you
+	    // will lose access to the result.
+	    end_result = soap.endWorkflow(token);
+	}
     }
 }
